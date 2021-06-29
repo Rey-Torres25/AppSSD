c1202be06d7deee7fa07268f9e5b8b9b013d7f1d
Gerardo Flores <gtm_hde@hotmail.com>
2021-06-29 16:09:50 -0500

    Quinceavo commit

--- app/src/main/java/com/ssd/appssd/fragmentsAdmin/PerfilFragmentAdmin.java
+++ app/src/main/java/com/ssd/appssd/fragmentsAdmin/PerfilFragmentAdmin.java
@@ -1,5 +1,6 @@
 package com.ssd.appssd.fragmentsAdmin;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,10 +8,21 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.ssd.appssd.MainActivity;
 import com.ssd.appssd.R;
 
 public class PerfilFragmentAdmin extends Fragment {
+
+    private Button btnLogOut;
+    private FirebaseAuth mAuth;
+    private StorageReference mStorage;
+    private ImageView photo;
 
     public PerfilFragmentAdmin() {
         // Required empty public constructor
@@ -22,6 +34,19 @@ public class PerfilFragmentAdmin extends Fragment {
         // Inflate the layout for this fragment
 
         View view = inflater.inflate(R.layout.fragment_perfil_admin, container, false);
+        btnLogOut = (Button) view.findViewById(R.id.btnLogOut);
+        mAuth = FirebaseAuth.getInstance();
+        mStorage = FirebaseStorage.getInstance().getReference();
+
+        btnLogOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mAuth.signOut();
+                Intent intent = new Intent(getActivity(), MainActivity.class);
+                getActivity().finish();
+                getActivity().startActivity(intent);
+            }
+        });
         return view;
     }
 }
